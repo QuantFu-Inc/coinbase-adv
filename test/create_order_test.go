c807be1f765f5c4316b4f0da69d44b7d94cf3f56
JasonD <jason@quantfu.com>
2023-01-27 17:32:07 -0500

    added CreateLimitOrder.

--- test/create_order_test.go
+++ test/create_order_test.go
@@ -66,5 +66,62 @@ func Test_CreateMarketOrder(t *testing.T) {
 		}
 
 	}
+}
+
+func Test_CreateLimitOrder(t *testing.T) {
+	//devToken := os.Getenv("CB-ACTOKEN")
+	//creds := client.Credentials{AccessToken: devToken}
+
+	creds := client.Credentials{
+		ApiKey:      os.Getenv("CB-APIKEY"),
+		ApiSKey:     os.Getenv("CB-SKEY"),
+		AccessToken: os.Getenv("CB-ACTOKEN"),
+	}
+
+	cln := client.NewClient(&creds)
+
+	cloid := fmt.Sprintf("unit-test-cmo-%d", time.Now().UnixMilli())
+	prod := "ETH-USD"
+	side := string(model.BUY)
+	baseSz := "0.01"
+	lmt := "1500"
+
+	p := &model.CreateOrderRequest{
+		ClientOrderId: &cloid,
+		ProductId:     &prod,
+		Side:          &side,
+		OrderConfiguration: &model.CreateOrderRequestOrderConfiguration{
+			LimitLimitGtc: &model.CreateOrderRequestOrderConfigurationLimitLimitGtc{
+				LimitPrice: &lmt,
+				BaseSize:   &baseSz,
+			},
+		},
+	}
+
+	rsp, err := cln.CreateOrder(p)
+	if err != nil {
+		fmt.Println(err)
+		t.FailNow()
+		return
+	}
+
+	if *rsp.Success {
+
+		p := client.ListOrdersParams{
+			OrderStatus: []string{string(model.OPEN)},
+		}
+
+		rsp, err := cln.ListOrders(&p)
+		if err != nil {
+			fmt.Println(err)
+			t.FailNow()
+			return
+		}
+
+		for _, o := range rsp.Orders {
+			fmt.Printf("[%s] [%s] [%s] %f  @ %f \n", o.GetStatus(), o.GetProductId(), o.GetSide(), o.GetFilledSize(), o.GetAverageFilledPrice())
+		}
+
+	}
 
 }
